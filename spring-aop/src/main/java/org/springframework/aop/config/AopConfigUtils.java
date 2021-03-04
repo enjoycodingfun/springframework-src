27e319ac66c0cfd891bfe9b9654bc8b0593130fe
running <627882647@qq.com>
2021-03-04 23:17:17 +0800

    SpringAOP源码

--- spring-aop/src/main/java/org/springframework/aop/config/AopConfigUtils.java
+++ spring-aop/src/main/java/org/springframework/aop/config/AopConfigUtils.java
@@ -119,7 +119,7 @@ public abstract class AopConfigUtils {
 			Class<?> cls, BeanDefinitionRegistry registry, @Nullable Object source) {
 
 		Assert.notNull(registry, "BeanDefinitionRegistry must not be null");
-
+       //如果已经存在了自动代理创建器且存在的自动代理创建器与现在的不一致，那么需要根据优先级来判断到底需要使用哪种
 		if (registry.containsBeanDefinition(AUTO_PROXY_CREATOR_BEAN_NAME)) {
 			BeanDefinition apcDefinition = registry.getBeanDefinition(AUTO_PROXY_CREATOR_BEAN_NAME);
 			if (!cls.getName().equals(apcDefinition.getBeanClassName())) {
